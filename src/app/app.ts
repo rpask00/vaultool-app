7c935d4d60ef085deb3d3dd58e869f7ad146f32a
Rafal Pasko <rp.261000@gmail.com>
2026-03-10 19:30:39 +0100

    add angular material.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ItemFormComponent } from './item-form/item-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ItemFormComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('vaultility-www');
-}
+export class App {}
--- src/app/item-form/item-form.component.ts
+++ src/app/item-form/item-form.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-item-form',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './item-form.component.html',
-  styleUrl: './item-form.component.css'
+  styleUrl: './item-form.component.css',
 })
 export class ItemFormComponent {
-
+  readonly formOpen = signal(false);
 }
